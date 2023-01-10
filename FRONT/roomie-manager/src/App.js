2cbb2ac79f56ac736b29c3703407946533c064b2
yasshabibi <72918175+yasshabibi@users.noreply.github.com>
2023-01-10 10:03:30 +0100

    initial commit

---
+++ FRONT/roomie-manager/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+import { Title1 } from './components/title';
+import { Bouton } from './components/bouton';
+
+
+function App() {
+  return (
+    <div className="App">
+        <Title1/>
+        <Bouton/>
+    </div>
+  );
+}
+
+export default App;
---
+++ FRONT/roomie-manager/src/components/title.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export function Title1(){
+    
+    return(
+
+        <>
+        <h1>Test</h1>
+        <p>Test Test</p>
+        </>
+    )
+    
+}
+
