a87947aaff6004b1004a694268140e4c11554530
B E N <93186925+BenIzak@users.noreply.github.com>
2023-01-11 08:56:56 +0100

    FRONT : get input info

--- project/src/components/registration/registration.jsx
+++ project/src/components/registration/registration.jsx
@@ -1,9 +1,39 @@
-import React from "react";
+import React,{useState} from "react";
 import { NavLink } from 'react-router-dom'
 
 import './registration.css';
 
 export function Register() {
+  
+  const [userName, setUserName] = useState('');
+  const [userPassword, setUserPassword] = useState('');
+
+  const handleUserNameChange = (event) => { // permet de changer la valeur de l'input
+    setUserName(event.target.value);
+  }
+
+  const handleUserPasswordChange = (event) => { // permet de changer la valeur de l'input
+    setUserPassword(event.target.value);
+  }
+
+  const handleSubmit = () => {
+    if (userName.length === 0) {
+      alert('Please enter user name');
+      return;
+    }
+    else if (userPassword.length === 0) {
+      alert('Please enter password');
+      return;
+    }
+
+    else if (userName.length > 0 && userPassword.length > 0){
+      alert('try to register');
+    }
+
+    alert(`User name: ${userName}, password: ${userPassword}`);
+  }
+
+
   return (
     <>
     <div className="login">
@@ -11,13 +41,13 @@ export function Register() {
         <form>
             <div className="form-group">
                 <label for="userName">User name</label>
-                <input type="text" className="form-control" id="userName" aria-describedby="userName" placeholder="Enter user name"/>
+                <input type="text" className="form-control" id="userName" value={userName} onChange={handleUserNameChange} aria-describedby="userName" placeholder="Enter user name"/>
             </div>
             <div className="form-group">
-                <label for="exampleInputPassword1">Password</label>
-                <input type="password" className="form-control" id="userPassword" placeholder="Password"/>
+                <label for="userPassword">Password</label>
+                <input type="password" className="form-control" id="userPassword" value={userPassword} onChange={handleUserPasswordChange} placeholder="Password"/>
             </div>
-            <button type="submit" className="submit">Register</button>
+            <button type="submit" className="submit" onClick={handleSubmit} >Register</button>
         </form>
     </div>
     </>
@@ -25,6 +55,36 @@ export function Register() {
 }
 
 export function Login() {
+
+  const [userName, setUserName] = useState('');
+  const [userPassword, setUserPassword] = useState('');
+
+  const handleUserNameChange = (event) => {
+    setUserName(event.target.value);
+  }
+
+  const handleUserPasswordChange = (event) => {
+    setUserPassword(event.target.value);
+  }
+
+  const handleSubmit = () => {
+    if (userName.length === 0) {
+      alert('Please enter user name');
+      return;
+    }
+    else if (userPassword.length === 0) {
+      alert('Please enter password');
+      return;
+    }
+
+    else if (userName.length > 0 && userPassword.length > 0){
+      alert('try to login');
+    }
+
+    alert(`User name: ${userName}, password: ${userPassword}`);
+  }
+
+
   return (
     <>
     <div className="register">
@@ -32,13 +92,13 @@ export function Login() {
         <form>
           <div className="form-group">
               <label for="userName">User name</label>
-              <input type="text" className="form-control" id="userName" aria-describedby="userName" placeholder="Enter user name"/>
+              <input type="text" className="form-control" id="userName" value={userName} onChange={handleUserNameChange} aria-describedby="userName" placeholder="Enter user name"/>
             </div>
             <div className="form-group">
-              <label for="exampleInputPassword1">Password</label>
-              <input type="password" className="form-control" id="userPassword" placeholder="Password"/>
+              <label for="userPassword">Password</label>
+              <input type="password" className="form-control" id="userPassword"  value={userPassword} onChange={handleUserPasswordChange} placeholder="Password"/>
             </div>
-          <button type="submit" className="submit">Register</button>
+          <button type="submit" className="submit" onClick={handleSubmit}>Register</button>
         </form>
     </div>
     </>
